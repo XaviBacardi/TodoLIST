e17184ca61482fb08d94d0a4966be4ff42eeb5a1
Xavi <erx_loboa@unicah.edu>
2022-08-08 20:37:08 -0600

    Login Succesful

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { RegistrarUsuario } from 'src/DTO/registrarUsuario.dto';
+import { UserLoginDto } from 'src/DTO/userLogin.dto';
 import { AuthService } from './auth.service';
 
 
@@ -10,9 +11,18 @@ export class AuthController {
     constructor(private authService: AuthService){
 
     }
+
+    //register a new user
     @Post('register')
     registration(@Body(ValidationPipe) regDTO: RegistrarUsuario){
         return this.authService.registerUser(regDTO);
+    }
+
+    //@Post como decorador y entre parentesis el nombre del enpoint API
+    @Post('login')
+    singin(@Body(ValidationPipe) loginDto: UserLoginDto){
+
+        return this.authService.loginUser(loginDto);
     }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,22 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegistrarUsuario } from 'src/DTO/registrarUsuario.dto';
 import { UserEntity } from 'src/Entity/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
+import { UserLoginDto } from 'src/DTO/userLogin.dto';
 
 
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity> ){
+    constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity> )
+    {
 
     }
 
-   async registerUser(resgisterDTO: RegistrarUsuario){
+   async registerUser(resgisterDTO: RegistrarUsuario)
+   {
         const {username, password} = resgisterDTO;
         const hashed = await bcrypt.hash(password, 12);
         const salt = await bcrypt.getSalt(hashed);
@@ -32,5 +35,32 @@ export class AuthService {
         }
 
        
+    }
+
+
+    //funcion asincrona que simula el logeo de un usuario al sistema haciendo match con sus credenciales
+    async loginUser(userLoginDTO: UserLoginDto)
+    {
+
+        const {username, password} = userLoginDTO;
+
+        //instancia en una constante para acceder a sus propiedades en db
+        const user = await this.repo.findOne({where: {username: username}})
+
+        if(!user)
+        {
+            throw new UnauthorizedException('Credenciales no validas');
+        }
+
+        const salt = user.salt;
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if(isPasswordMatch)
+        {
+            return {message: 'Logeado exitosamente'}
+        }else{
+            throw new UnauthorizedException('Credenciales no validas');
+        }
+
     }
 }
